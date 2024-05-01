a56c157da2d4c5637550b1f718ba4e4cd874774c
Swapnil Mukherjee <swapnilmukherjee24@gmail.com>
2024-05-01 13:46:37 -0400

    updaed main.py and added .config file

--- application/main.py
+++ application/main.py
@@ -1,10 +1,12 @@
 """FastAPI starter sample
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Response, status
+from fastapi.security import HTTPBearer
+from application.utils import VerifyToken
 
 app = FastAPI()
-
+token_auth_scheme = HTTPBearer()
 
 @app.get("/api/public")
 def public():
@@ -19,14 +21,19 @@ def public():
 
 
 @app.get("/api/private")
-def private():
+def private(token: str = Depends(token_auth_scheme)):
     """A valid access token is required to access this route"""
+
+    result = VerifyToken(token.credentials).verify()
+
+    if result.get("status") == "error":
+        return Response(content=result, status_code=status.HTTP_403_FORBIDDEN)
 
     # We need to protect this endpoint!
 
-    result = {
-        "status": "success",
-        "msg": ("Hello from a private endpoint! "
-                "You need to be authenticated to see this.")
-    }
+    # result = {
+    #     "status": "success",
+    #     "msg": ("Hello from a private endpoint! "
+    #             "You need to be authenticated to see this.")
+    # }
     return result
